6767c93344b7c0084188bd6153e7fee1b33baa48
ASHICK <rainaashick231173@gmail.com>
2024-12-26 06:57:59 +0000

    Hooks-Concept

---
+++ Introduction-to-react-hooks-and-state-hook/Coding-Practices/Read-More-Less/src/components/ReadMore/index.js
@@ -0,0 +1,40 @@
+import {useState} from 'react'
+import {
+  MainContainer,
+  Heading,
+  Details,
+  Description,
+  Image,
+  Button,
+  Own,
+} from './styledComponents'
+
+const ReadMore = props => {
+  const {reactHooksDescription} = props
+  // console.log(reactHooksDescription)
+  const shortText = reactHooksDescription.slice(0, 170)
+  const [buttonText, setButton] = useState(false)
+
+  const onClickRead = () => {
+    setButton(prevState => !prevState)
+  }
+
+  const readText = buttonText ? 'Read Less' : 'Read More'
+  const descriptionText = buttonText ? reactHooksDescription : shortText
+
+  return (
+    <MainContainer>
+      <Heading>React Hooks</Heading>
+      <Own>Designed by Ashiq</Own>
+      <Details>Hooks are a new addition to React</Details>
+      <Image
+        src="https://assets.ccbp.in/frontend/hooks/react-hooks-img.png"
+        alt="react hooks"
+      />
+      <Description>{descriptionText}</Description>
+      <Button onClick={onClickRead}>{readText}</Button>
+    </MainContainer>
+  )
+}
+
+export default ReadMore
